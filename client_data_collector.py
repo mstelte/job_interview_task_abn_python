59188d567f64b40c5c04f5414b1ff857b8cd65d9
marcel_s <marcel.stelte@gmail.com>
2023-09-13 09:43:10 +0200

    Implemented argparse for arguments.

--- client_data_collector.py
+++ client_data_collector.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import argparse
 
 def read_data(filename, selector_columns: list, selector_values: list):
     '''Take the name of a csv file, a list of columns to be selected, and a list with filter values for each selected column, and return the corresponding rows as a Pandas DataFrame.
@@ -30,11 +31,42 @@ def read_data(filename, selector_columns: list, selector_values: list):
 
     return data
 
+def get_params():
+    '''Return the user's input parameters as a tuple (file 1, file 2, countries).'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f1',
+        '--file1',
+        nargs=1,
+        type=str,
+        help='Name of the first file to be read.'
+    )
+    parser.add_argument(
+        '-f2',
+        '--file2',
+        nargs=1,
+        type=str,
+        help='Name of the second file to be read.'
+    )
+    parser.add_argument(
+        '-c',
+        '--countries',
+        nargs='*',
+        type=str,
+        help='List of the relevant countries. Country names with multiple names have to be written with underscore instead of space.'
+    )
+    args = parser.parse_args()
+
+    countries = [c.replace('_', ' ') for c in args.c]
+    return (args.f1[0], args.f2[0], countries)
+
 
 
 __name__ == '__main__'
-data_1 = read_data("dataset_one.csv", ["id", "email", "country"], [[], [], ["United Kingdom", "Netherlands"]])
-data_2 = read_data("dataset_two", ["id", "btc_a", "cc_t"], [data_1['id'].tolist(), [], []])
+file_1, file_2, countries = get_params()
+
+data_1 = read_data(file_1, ["id", "email", "country"], [[], [], countries])
+data_2 = read_data(file_2, ["id", "btc_a", "cc_t"], [data_1['id'].tolist(), [], []])
 data_1 = data_1.rename(columns={'id': 'client_identifier'})
 data_2 = data_2.rename(columns={'id': 'client_identifier', 'btc_a': 'bitcoin_address', 'cc_t': 'credit_card_type'})
 df = pd.merge(data_1, data_2, on='client_identifier')
